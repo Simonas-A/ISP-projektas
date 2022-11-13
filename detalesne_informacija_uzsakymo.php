f5c7a8370d76ffd2da5affe1d1c377e7884a48cf
vytcka <65495256+vytcka@users.noreply.github.com>
2022-11-13 02:59:23 +0200

    Add files via upload

---
+++ detalesne_informacija_uzsakymo.php
@@ -0,0 +1,82 @@
+
+
+<html>  
+      <head>  
+           <title>Peržiūrėjimas apklausos temų</title>  
+           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		   <title>Užsakymo redagavimas</title>
+  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		   <link rel="stylesheet" href="css/style.css">  
+
+		   <style>
+				h5.align-left {
+					text-align:left;
+					padding-left: 40px;
+				}
+			</style>
+      </head>  
+ </html> 
+
+<?php
+	session_start();
+	include "include/nustatymai.php";
+
+	$userlevel=$_SESSION['ulevel'];
+	$previous_page = $_SESSION['prev'];
+	$_SESSION['prev'] = 'detalesne_informacija_uzsakymo';
+    $id = $_GET['id'];
+
+    echo "<table width=100% border=\"0\" cellspacing=\"1\" cellpadding=\"3\" class=\"meniu\">";
+	echo "<tr><td>";
+	echo "</td></tr><tr><td>";
+	echo "<form method='post' action='".$previous_page.".php' style='display:inline'><input type='submit' value='Grįžti' class='btn btn-danger' name='btn1'></form> &nbsp;&nbsp;";
+    echo "</td></tr></table>";
+
+	echo "<center><div style='width:50%; display:inline-block;'>";
+	echo "<h1 style='text-aling:left'>Užsakymo ID: ".$id." detalesnė informacija</h1><br>";
+	$dbc= new mysqli("localhost", "stud", "stud", "vartvald");
+	$userid = $_SESSION['userid'];
+	$sql = "SELECT p.id, p.data, p.kaina, p.prekiu_kiekis, p.fk_pristatymo_id, budai.budas  FROM pirkimai AS p INNER JOIN apmokejimas AS a ON p.fk_apmokejimo_budo_id = a.id INNER JOIN apmokejimo_budai AS budai ON a.apmokejimo_budas = budai.id WHERE p.id = $id ORDER BY p.data DESC";
+    
+	$result = mysqli_query($dbc, $sql);
+	
+	while($row = mysqli_fetch_assoc($result))
+	{
+		echo "<h5 class='align-left'>Užsakymo data: ".$row['data']."</h3><br>";
+		echo "<h5 class='align-left'>Bendra užsakymo kaina: ".$row['kaina']."</h3><br>";
+		echo "<h5 class='align-left'>Bendras prekių kiekis: ".$row['prekiu_kiekis']."</h3><br>";
+		echo "<h5 class='align-left'>Mokėjimo būdas: ".$row['budas']."</h3><br>";
+
+		$shippment_id = $row['fk_pristatymo_id'];
+		$sql_for_shippment_information = "SELECT * FROM pristatymai INNER JOIN statusai ON pristatymai.statusas = statusai.id WHERE pristatymai.id = $shippment_id";
+    	$array_result_shippment = mysqli_query($dbc, $sql_for_shippment_information);
+		$shippment_information = mysqli_fetch_assoc($array_result_shippment);
+
+		echo "<h5 class='align-left'>Užsakymo statusas: ".$shippment_information['statusas']."</h3><br>";
+		echo "<h5 class='align-left'>Pristatymo adresas: ".$shippment_information['adresas']."</h3><br>";		
+	} 
+
+	$sql = "SELECT prekes.pavadinimas, prekes.kaina, ppt.pirktas_kiekis FROM prekes INNER JOIN preke_pirkimai_tarpinis AS ppt ON prekes.id = ppt.fk_preke_id WHERE ppt.fk_pirkimas_id = $id";
+	$result = mysqli_query($dbc, $sql);
+
+	echo "<center><h1>Užsakytos prekės</h1><br></center>";
+	echo "<table style='width:50%' align='center' class='table table-bordered table-dark'>";
+	echo "<tr>
+	<th>Užsakyta prekė</th>
+	<th>Kiekis</th>
+	<th>Kaina</th>
+	</tr>";
+	while($row = mysqli_fetch_assoc($result))
+	{
+		$total_price = $row['pirktas_kiekis'] * $row['kaina'];
+		echo "<tr><td>".$row['pavadinimas']."</td>";
+		echo "<td>".$row['pirktas_kiekis']."</td>";
+		echo "<td>".$total_price."</td>";
+	} 
+	echo "</table>";
+
+	echo "</div></center>";
+
+	
+?>
---
+++ uzsakymo_info_keitimas.php
@@ -0,0 +1,113 @@
+<html>  
+      <head>  
+           <title>Peržiūrėjimas apklausos temų</title>  
+           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		   <title>Užsakymo informacija</title>
+  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		   <link rel="stylesheet" href="css/style.css">  
+		   <script src="path-to/js/bootstrap-select.min.js"></script>
+
+		   <style>
+				h5.align-left {
+					text-align:left;
+					padding-left: 60px;
+				}
+			</style>
+      </head>  
+ </html> 
+
+<?php
+	session_start();
+	include "include/nustatymai.php";
+
+	$userlevel=$_SESSION['ulevel'];
+	$previous_page = $_SESSION['prev'];
+
+	$id = $_GET['id'];
+
+    echo "<table width=100% border=\"0\" cellspacing=\"1\" cellpadding=\"3\" class=\"meniu\">";
+	echo "<tr><td>";
+	echo "</td></tr><tr><td>";
+	echo "<form method='post' action='".$previous_page.".php' style='display:inline'><input type='submit' value='Grįžti' class='btn btn-danger' name='btn1'></form> &nbsp;&nbsp;";
+    echo "</td></tr></table>";
+
+	echo "<center><div class='container mt-5'>";
+	echo "<h1 style='text-aling:left'>Užsakymo ID: ".$id." informacijos redagavimas</h1><br>";
+	$dbc= new mysqli("localhost", "stud", "stud", "vartvald");
+	$userid = $_SESSION['userid'];
+	$sql = "SELECT p.id, p.data, p.kaina, p.prekiu_kiekis, p.fk_pristatymo_id, budai.budas FROM pirkimai AS p INNER JOIN apmokejimas AS a ON p.fk_apmokejimo_budo_id = a.id INNER JOIN apmokejimo_budai AS budai ON a.apmokejimo_budas = budai.id WHERE p.id = $id ORDER BY p.data DESC";
+	$result = mysqli_query($dbc, $sql);
+	
+	while($row = mysqli_fetch_assoc($result))
+	{
+		echo "<form method='post'><div><label>Pristatymo norimas laikas</label><input type='datetime-local' class='form-control' name='data' placeholder='Pristatymo normas laikas' value='".$row['data']."'/></div>";
+		echo "<div class='form-group'><label>Bendra užsakymo kaina</label><input type='text' class='form-control' name='price' placeholder='Bendra užsakymo kaina' value='".$row['kaina']."' /></div>";
+		echo "<div class='form-group'><label>Bendras prekių kiekis</label><input type='text' class='form-control' name='count' placeholder='Bendras prekių kiekis' value='".$row['prekiu_kiekis']."' /></div>";
+
+		echo "<div class='form-group'><label>Mokėjimo būdas</label><select class='form-control' name='budas' id='budas' multiple>";
+		$sql_for_selection_options = "SELECT * FROM apmokejimo_budai";
+		$select_options = mysqli_query($dbc, $sql_for_selection_options);
+		$number = 1;
+		while($options = mysqli_fetch_assoc($select_options)) {
+			if($row['budas'] != $options['budas']) {
+				echo "<option value='".$number."'>".$options['budas']."</option>";
+			}
+			else {
+				echo "<option value=".$number." selected>".$options['budas']."</option>";
+			}
+
+			$number++;
+		}
+		echo "</select></div>";
+		
+		
+
+		$shippment_id = $row['fk_pristatymo_id'];
+		$sql_for_shippment_information = "SELECT * FROM pristatymai INNER JOIN statusai ON pristatymai.statusas = statusai.id WHERE pristatymai.id = $shippment_id";
+    	$array_result_shippment = mysqli_query($dbc, $sql_for_shippment_information);
+		$shippment_information = mysqli_fetch_assoc($array_result_shippment);
+
+		echo "<div class='form-group'><label>Užsakymo statusas</label><select class='form-control' name='statusas' id='statusas' multiple>";
+		$sql_for_selection_options = "SELECT * FROM statusai";
+		$select_options = mysqli_query($dbc, $sql_for_selection_options);
+		$number = 1;
+		while($options = mysqli_fetch_assoc($select_options)) {
+			if($shippment_information['statusas'] != $options['statusas']) {
+				echo "<option value='".$number."'>".$options['statusas']."</option>";
+			}
+			else {
+				echo "<option value='".$number."' selected>".$options['statusas']."</option>";
+			}
+
+			$number++;
+		}
+		echo "</select></div>";
+		
+		echo "<div class='form-group'><label>Pristatymo adresas</label><input type='text' class='form-control' name='adresas' placeholder='Pristatymo adresas' value='".$shippment_information['adresas']."' /></div>";		
+	} 
+
+	echo "<div class='form-group'><input type='submit' value='Atnaujinti' name='btn' class='btn btn-danger'></div></form>";
+
+	if(isset($_POST['btn']))
+    {
+		$_SESSION['prev'] = 'uzsakymo_info_keitimas';
+        $datetime = $_POST['data'];
+        $total_price = $_POST['price'];
+        $count_of_items = $_POST['count'];
+        $payment_type = $_POST['budas'];
+        $purchase_status = $_POST['statusas'];
+		$shippment_address = $_POST['adresas'];
+
+		$sql_for_purchases = "UPDATE pirkimai SET kaina = '$total_price', prekiu_kiekis = '$count_of_items', fk_apmokejimo_budo_id = '$payment_type' WHERE id = $id";
+		$query = mysqli_query($dbc, $sql_for_purchases);
+
+		$sql_for_shippment = "UPDATE pristatymai SET data = '$datetime', statusas = '$purchase_status', adresas = '$shippment_address' WHERE id = $id";
+        $query=mysqli_query($dbc, $sql_for_shippment);
+
+		$_SESSION['message'] = 'Sėkmingai atnaujinta';
+		$_SESSION['header'] = 'yes';
+
+        header("location:".$previous_page.".php");
+    } 
+?>
